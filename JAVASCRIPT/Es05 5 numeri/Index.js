4b0e4ac5f9660814c4861aacb185c68616ca0493
pierettoFrancesco <f.pieretto.2292@vallauri.edu>
2022-02-07 19:08:10 +0100

    compiti

--- JAVASCRIPT/Es05 5 numeri/Index.js
+++ JAVASCRIPT/Es05 5 numeri/Index.js
@@ -1,5 +1,23 @@
 "use strict"
 
+let _nbr=document.getElementsByName("txtNum");
+let _chkbox=document.getElementsByName("chkNum");
+let cont=[];
+
+function init(){
+
+    cont[0]=generaNumero(0,6);
+    for(let i=1;i<6;i++)
+    {
+        cont[i]=generaNumero(0,6);
+        while()
+    }
+        
+    console.log(cont);
+
+
+}
+
 function generaNumero(a,b){
     /* estremo sueriore escluso*/
     let ris=Math.floor((b-a)*Math.random())+a;
