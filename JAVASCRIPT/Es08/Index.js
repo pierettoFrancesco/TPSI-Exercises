2a961e881086cb09271f887993fca54104d3170a
pierettoFrancesco <f.pieretto.2292@vallauri.edu>
2022-02-14 19:10:55 +0100

    lezione

--- JAVASCRIPT/Es08/Index.js
+++ JAVASCRIPT/Es08/Index.js
@@ -8,12 +8,14 @@ let _txtCar;
 let _chkRis;
 let _btnInvia;
 let _btnRisposta;
+let _txtIns;
 
 function inizializza(){
 
     let pos=generaNumero(0., vet.length);
     parolaSegreta=vet[pos];
     parolaSegreta=parolaSegreta.toUpperCase();
+    console.log(parolaSegreta);
 
     _txtPunti=document.getElementById("txtPunti");
     _txtPunti.value=punti;  //conversione stringa fatta in automatico
@@ -21,6 +23,7 @@ function inizializza(){
     _chkRis=document.getElementsByName("chkRis");
     _btnInvia=document.getElementById("btnInvia");
     _btnRisposta=document.getElementById("btnRisposta");
+    _txtIns=document.getElementById("txtIns");
 
     for(let i=0;i<_txtCar.length;i++)
     {
@@ -36,6 +39,84 @@ function inizializza(){
     _btnInvia.disabled=false;
     _btnRisposta.disabled=false;
 
+}
+
+function confronta(){
+
+    let i=0;
+    let carattere=_txtIns.value;
+    if(carattere.length==1)
+    {
+       
+        carattere=carattere.toUpperCase();
+        for(i=0;i<parolaSegreta.length;i++)
+        {
+            if(carattere==parolaSegreta[i])
+            {
+                _txtCar[i].value=carattere;
+                _chkRis[i].checked=true;
+            }
+        }
+        punti-=5;
+        _txtPunti.value=punti;
+        while(i<parolaSegreta.length && _chkRis[i].checked==true)
+            i++;
+        if(i==parolaSegreta.length && punti>0)
+        {
+            alert("Hai vinto");
+            _btnInvia.disabled=true;
+            _btnRisposta.disabled=true;
+        }
+        else if(punti<=0)
+        {
+            alert("Hai perso");
+            _btnInvia.disabled=true;
+            _btnRisposta.disabled=true;
+        }
+            
+    }
+    else
+    {
+        alert("METTI UN FORMATO VALIDO ")
+        _txtIns.value="";
+    }
+}
+
+function rispondi(){
+
+    let parola=_txtIns.value;
+
+    if(parola!="")
+    {
+        parola=parola.toUpperCase();
+
+        if(parola==parolaSegreta && punti>0)
+        {
+            for(let i=0;i<parolaSegreta.length;i++){
+                _txtCar[i].value=parola[i];
+                _chkRis[i].checked=true;
+            }
+            alert("Hai vinto");
+            _btnInvia.disabled=true;
+            _btnRisposta.disabled=true;
+        }
+        else 
+        {
+            alert("riprovare");
+             punti-=20;
+             _txtPunti.value=punti;
+        }
+        if(punti<=0){
+            alert("Hai perso");
+            _btnInvia.disabled=true;
+            _btnRisposta.disabled=true;
+        }
+    }
+    else
+    {
+        alert("METTI UN FORMATO VALIDO ")
+        _txtIns.value="";
+    }
 }
 
 function generaNumero(a,b){
