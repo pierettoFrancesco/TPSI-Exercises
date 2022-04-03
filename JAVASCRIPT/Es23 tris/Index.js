0bd5dbcefe5c2f789a0dfb95507dcf4e87e8625e
pierettoFrancesco <f.pieretto.2292@vallauri.edu>
2022-04-03 17:44:58 +0200

    compiti

--- JAVASCRIPT/Es23 tris/Index.js
+++ JAVASCRIPT/Es23 tris/Index.js
@@ -1,19 +1,18 @@
 "use strict"
 
 const dim=3;
-let i;
-let j;
+let cont;
 
 window.onload=function(){
 
     let _table=document.getElementsByTagName("table")[0];
     let _ris=document.getElementById("divRisultato");
-    let cont=0;
+    cont=0;
 
-    for(i=0;i<dim;i++)
+    for(let i=0;i<dim;i++)
     {
         let _tr=document.createElement("tr");
-        for(j=0;j<dim;j++)
+        for(let j=0;j<dim;j++)
         {
             let _td=document.createElement("td");
             let _img=document.createElement("img");
@@ -21,34 +20,32 @@ window.onload=function(){
             _img.id="img-"+i+"-"+j;
             _img.innerHTML="&nbsp";
             _td.appendChild(_img);
+            _img.src="img/vuota.png";
             _img.addEventListener("click", imgClick);
             _tr.appendChild(_td);
-            _img.src="img/vuota.png";
+            console.log(_img);
         }
         _table.appendChild(_tr);
     }
 
     function imgClick(){
         
-        let aus = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
         if(cont%2==0)
         {
-            this.src="img/croce.png";
-            cont++;
-            aus[i,j]=1;
+            if(this.src=="img/vuota.png")
+            {
+                this.src="img/croce.png";
+                cont++;
+            }
         }
         else
         {
-            this.src="img/cerchio.png";
-            cont++;
-            aus[i,j]=2;
+            if(this.src=="img/vuota.png")
+            {
+                this.src="img/cerchio.png";
+                cont++;
+            }
         }
-        this.disabled=true;
-        if(aus[0,0]==1 && aus[0,1]==1 && aus[0,2]==1)
-            _ris.innerHTML="giocatore 1";
-        else if(aus[0,0]==2 && aus[0,1]==2 && aus[0,2]==2)
-            _ris.innerHTML="giocatore 2";
-        
         if(cont==9){
             _ris.innerHTML="PAREGGIO";
         }
